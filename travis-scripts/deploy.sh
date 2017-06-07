3c9938cf4e8553f800c1cba5f06aae9cb5f6f490
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-06-07 12:41:25 -0400

    Experiment Allowing Failed Deploys

--- travis-scripts/deploy.sh
+++ travis-scripts/deploy.sh
@@ -2,12 +2,18 @@
 
 if [ $TRAVIS_PULL_REQUEST != 'false' ]; then
     echo "Pull Requests are not released"
+    export TRAVIS_ALLOW_FAILURE=true
+    false
 
 elif [ $TRAVIS_REPO_SLUG != "http4s/blaze" ]; then
     echo "Only Http4s/Blaze Repository Code is Released"
+    export TRAVIS_ALLOW_FAILURE=true
+    false
 
 elif [ $TRAVIS_BRANCH != "master" ]; then
     echo "Only Master Branch is Released"
+    export TRAVIS_ALLOW_FAILURE=true
+    false
 
 else
    sudo chmod +x /usr/local/bin/sbt # Temporary Fix For https://github.com/travis-ci/travis-ci/issues/7703
